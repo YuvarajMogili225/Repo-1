dfb67e47a9535d54b1216796c206e467d3d5603a
YuvarajMogili1993 <mogiliyuvaraju1993@gmail.com>
2021-04-14 12:10:00 +0530

    Greentech.V1 commit

---
+++ Greentechv1.4/src/main/java/GreenTech/App.java
@@ -0,0 +1,13 @@
+package GreenTech;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
